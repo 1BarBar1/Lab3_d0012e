fe8e1e8cc3b2d4335f4065a2defb3c7cb75ae029
1BarBar1 <harrygrehag@gmail.com>
2023-12-06 15:58:09 +0100

    Node class and main file

---
+++ Main.py
@@ -0,0 +1,6 @@
+from Node import Node
+root = Node(10)
+root.add_node(11)
+root.add_node(8)
+print('Left',root.left_child.size)
+print('right',root.right_child.size)
---
+++ Node.py
@@ -0,0 +1,34 @@
+class Node:
+    def __init__(self, size):
+        '''
+        self 
+        parent int: the size of the root node to the new node 
+        size int: the size of the node   
+        '''
+        self.size = size
+        self.left_child = None
+        self.right_child = None
+        self.c = None
+        
+    def add_node(self,size):
+        '''
+        Add a child node to the tree and iff the node is smaller the the root
+        it is the left child node else the right child node
+
+        size int: the size off the new node
+        '''
+        if size < self.size:
+            if self.left_child is not None:
+                self.left_child.add_node(size)
+            else:
+                self.left_child = Node(size)
+        elif size >= self.size:
+            if self.right_child is not None:
+                self.right_child.add_node(size)
+            else:
+                self.right_child = Node(size)
+
+    def give_nodesize(self):
+        return self.size
+        
+    
