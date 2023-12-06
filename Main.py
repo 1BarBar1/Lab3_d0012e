b5603251d3e5c3bc48bbb67c9bf5be9ea2ec813c
1BarBar1 <harrygrehag@gmail.com>
2023-12-06 19:34:37 +0100

    stuff

--- Main.py
+++ Main.py
@@ -1,6 +1,37 @@
 from Node import Node
-root = Node(10)
-root.add_node(11)
-root.add_node(8)
-print('Left',root.left_child.size)
-print('right',root.right_child.size)
+parent = Node(10)
+parent.add_node(11)
+parent.add_node(8)
+parent.add_node(12)
+parent.add_node(4)
+parent.add_node(10)
+
+
+print(parent.size)
+
+
+COUNT =[10]
+def print2DUtil(root, space):
+ 
+    # Base case
+    if (root == None):
+        return
+ 
+    # Increase distance between levels
+    space += COUNT[0]
+ 
+    # Process right child first
+    print2DUtil(root.right_child, space)
+ 
+    # Print current node after space
+    # count
+    print()
+    for i in range(COUNT[0], space):
+        print(end=" ")
+    print(root.value)
+ 
+    # Process left child
+    print2DUtil(root.left_child, space)
+ 
+# Wrapper over print2DUtil()
+print2DUtil(parent, 5)
--- Node.py
+++ Node.py
@@ -1,36 +1,43 @@
 class Node:
-    def __init__(self, size):
+    def __init__(self, value):
         '''
         self 
         parent int: the size of the root node to the new node 
         size int: the size of the node   
         '''
-        self.size = size
+        self.value = value
         self.left_child = None
         self.right_child = None
         self.c = 0.5
-        
-    def add_node(self,size):
+        self.size = 1
+    def add_node(self,value):
         '''
         Add a child node to the tree and iff the node is smaller the the root
         it is the left child node else the right child node
 
         size int: the size off the new node
         '''
-        if size < self.size and self.c*size < self.size:
-            if self.left_child is not None:
-                self.left_child.add_node(size)
-            else:
-                self.left_child = Node(size)
-        elif size >= self.size and self.c*size < self.size:
-            if self.right_child is not None:
-                self.right_child.add_node(size)
-            else:
-                self.right_child = Node(size)
-        elif self.c*size > self.size:
-            
-
-    def give_nodesize(self):
-        return self.size
+        self.size += 1
+        try: 
+            #self.c*self.size < self.left_child.size or self.c*self.size < self.left_child.size
+            X = None*2
+        except:
+            if value < self.value:
+                if self.left_child is not None:
+                    self.left_child.add_node(value)
+                else:
+                    self.left_child = Node(value)
+            elif value >= self.value:
+                if self.right_child is not None:
+                    self.right_child.add_node(value)
+                else:
+                    self.right_child = Node(value)
+        else:
+            print('error subtree to', self.size)
+            print(f'eq L: {self.c}*{self.size} > {self.left_child.size}')
+            print(f'eq R: {self.c}*{self.size} > {self.right_child.size}')
         
+        
+
+   
     
